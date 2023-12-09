082ee7e9f5ee4f490ee6c2bc0a76842e4673c57b
Ricardo Samedi <samedi.ricardo@gmail.com>
2023-12-09 13:13:57 -0500

    Creation du module de competences

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,11 +4,18 @@ $(function(){
       $(this).removeClass('active');
       $('header').removeClass('active');
       $('.main').removeClass('active');
+      $('.main-competences').removeClass('active');
     }
     else{
     $(this).addClass('active');
     $('header').addClass('active');
     $('.main').addClass('active');
+    $('.main-competences').addClass('active');
   }
+  });
+  /** Chanegement de lien active */
+  $('.menulink li a').on('click', function(){
+    $('.menulink li').removeClass('active');
+    $(this).parent().addClass('active')
   });
 });
